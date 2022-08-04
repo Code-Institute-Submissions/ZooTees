09a1d930aebbb9508919a2a284a658351812bed6
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-08-04 18:43:16 +0000

    fix typo in product model

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=254)
 
-    def __str_(self):
+    def __str__(self):
         return self.name
 
 
@@ -12,7 +12,7 @@ class Collection(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
 
-    def __str_(self):
+    def __str__(self):
         return self.name
 
 
