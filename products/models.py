91ff988dea5c69ccbbbc02b155b503a7016a2349
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-08-04 18:39:35 +0000

    remove friendly_name from category model

--- products/models.py
+++ products/models.py
@@ -3,18 +3,15 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str_(self):
         return self.name
-
-    def get_friendly_name(self):
-        return self.friendly_name
 
 
 class Collection(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
+
     def __str_(self):
         return self.name
 
