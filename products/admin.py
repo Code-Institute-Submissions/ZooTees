f5804c20fe32959500ad31dc8b3be7e1394e625e
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-08-07 17:01:48 +0000

    modify product admin list order

--- products/admin.py
+++ products/admin.py
@@ -3,7 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product, Category, Collection
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "sku",
+        "name",
+        "price",
+        "image",
+    )
+
+    ordering = ("sku",)
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Collection)
