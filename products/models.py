d2045fba27a66628cfafc434a36e7bea8f7379f7
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-08-07 18:35:58 +0000

    add image field to collection model

--- products/models.py
+++ products/models.py
@@ -11,6 +11,8 @@ class Category(models.Model):
 class Collection(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
