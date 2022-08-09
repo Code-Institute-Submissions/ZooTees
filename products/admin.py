21108923f30285006d08e5f98e286214e35f708d
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-08-09 17:40:55 +0000

    remove unused code, models

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, Category, Collection
+from .models import Product, Collection
 
 
 class ProductAdmin(admin.ModelAdmin):
--- products/models.py
+++ products/models.py
@@ -1,17 +1,9 @@
 from django.db import models
 
-# Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=254)
-
-    def __str__(self):
-        return self.name
-
 
 class Collection(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
@@ -19,9 +11,6 @@ class Collection(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(
-        "Category", null=True, blank=True, on_delete=models.SET_NULL
-    )
     collection = models.ForeignKey(
         "Collection", null=True, blank=True, on_delete=models.SET_NULL
     )
@@ -29,7 +18,6 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
