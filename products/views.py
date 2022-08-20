60162586c7ed3ce3d33ea13966e2b41a0a815186
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-08-20 19:16:54 +0000

    add product edit and delete buttons,views,template

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 """ product views """
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.db.models.functions import Lower
@@ -69,9 +70,12 @@ def product_detail(request, product_id):
 
     return render(request, "products/product_detail.html", context)
 
-
+@login_required
 def add_product(request):
     """Add a product to the store"""
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -92,3 +96,42 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+@login_required
+def edit_product(request, product_id):
+    """Edit a product in the store"""
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Successfully updated product!")
+            return redirect(reverse("product_detail", args=[product.id]))
+        else:
+            messages.error(
+                request,
+                "Failed to update product. Please ensure the form is valid.",
+            )
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f"You are editing {product.name}")
+
+    template = "products/edit_product.html"
+    context = {
+        "form": form,
+        "product": product,
+    }
+
+    return render(request, template, context)
+
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
